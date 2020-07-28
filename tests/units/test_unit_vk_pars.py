949b786c835f7faf546799a435ea8a6ae5546b54
raigor <raigor@mail.ru>
2020-07-29 00:16:16 +0700

    get packs from vk

---
+++ tests/units/test_unit_vk_pars.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestUnit(unittest.TestCase):
+   
+    def test_create_str(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
--- vk_pars.py
+++ vk_pars.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import io
-import vk
+#import vk
 import logging
-import requests
-import vk_api
-import bs4
+#import requests
+#import vk_api
+#import bs4
 import os
 import aiovk
 import aiohttp
 import asyncio
 import multiprocessing
+import time
 
 
 from optparse import OptionParser
@@ -21,30 +22,33 @@ class CreateTaskQueue(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.num_ids_chunk = num_ids_chunk
         self.start_ids = start_ids
+        print(self.start_ids)
         self.num_comm = num_comm
         self.req_per_sec = req_per_sec
         self.queue = queue
 
-    def create_ids_str(self, list_ids):
-        return ','.join(str(id) for id in list_ids)
+    def create_ids_str(self, start, n_id_ch,  r_p_sec):
+        return [','.join(str(start + n_id_ch * item_p + item_id) for item_id in range(n_id_ch)) for item_p in range(r_p_sec)]
 
     def run(self):
         print(self.name)
-        request_package = []
-        for item_id in range(self.num_comm//self.num_ids_chunk):
-            request_package.append(
-                [self.start_ids + item + item_id*self.num_ids_chunk for item in range(self.num_ids_chunk)]
-                )
-        else:
-            if self.num_comm % self.num_ids_chunk:
-                request_package.append(
-                    [self.start_ids + self.num_ids_chunk * (item_id + 1) + 1 for item in range(self.num_comm % self.num_ids_chunk)]
-                    )
-        for ind_list, item_id in enumerate(request_package):
-                if self.queue.qsize() < self.req_per_sec:
-                    self.queue.put(self.create_ids_str(request_package))
-                if not ind_list % self.req_per_sec and ind_list:
-                    time.sleep(1)
+             
+        start_id = self.start_ids
+        n = 1
+        while True:
+            if self.queue.qsize() < self.req_per_sec:
+                print()
+                request_package = self.create_ids_str(self.start_ids, self.num_ids_chunk, self.req_per_sec)
+                print(request_package, len(request_package))
+                for id_list in request_package:
+                    self.queue.put(id_list)
+                self.start_ids += self.num_ids_chunk * self.req_per_sec
+     
+            if self.start_ids > start_id + self.num_ids_chunk * (self.num_comm): 
+                print('-----------', n)
+                break
+                
+            time.sleep(1)
         
             
 class TaskVkPars(multiprocessing.Process):
@@ -54,25 +58,28 @@ class TaskVkPars(multiprocessing.Process):
         #self.session = session
         self.fields = fields
 
-    async def reqw(self):
+    async def reqw(self, strquer):
         
-        req = '1,2,3,4,5,6,7'
-        data = await self.api.groups.getById(group_ids=req, fields=self.fields)
-        print('\n')
-        print(data)
+        print(strquer, type(strquer))
+       
+        data = await self.api.groups.getById(group_ids=strquer, fields=self.fields)
+        print('\n', len(data))
+        #print(data)
 
     async def main(self):
         async with aiovk.TokenSession(access_token=token_read()) as session:
             self.api = aiovk.API(session)
             while True:
-                
+                strquer = self.queue.get()
+                if strquer is None:
+                    break
                 try:
-                    await asyncio.wait_for(self.reqw(), timeout=10)
+                    await asyncio.wait_for(self.reqw(strquer), timeout=10)
                 except asyncio.TimeoutError:
                     log.info('Error timeout request_period')
-                await asyncio.sleep(2)
+                await asyncio.sleep(1)
                 print(self.queue.qsize())
-                break
+               
         
     def run(self):
         self.proc_name = self.name
@@ -80,30 +87,6 @@ class TaskVkPars(multiprocessing.Process):
         asyncio.run(self.main())
         log.info('Will be create %s processes' % self.proc_name)
            
-            
-            
-
-
-
-def get_members(vk_api, groupid):  # Функция формирования базы участников сообщества в виде списка
-    first = vk_api.groups.getMembers(group_id=groupid, v=5.92)  # Первое выполнение метода
-    data = first["items"]  # Присваиваем переменной первую тысячу id'шников
-    count = first["count"] // 1000  # Присваиваем переменной количество тысяч участников
-    # С каждым проходом цикла смещение offset увеличивается на тысячу
-    # и еще тысяча id'шников добавляется к нашему списку.
-    for i in range(1, count+1):  
-        data = data + vk_api.groups.getMembers(group_id=groupid, v=5.92, offset=i*1000)["items"]
-    return data
-
-
-def vk_group_user(vk_api, name_user):
-    #s = vk_api.groups.getById(group_id=name_user, v=5.118)
-    #s = vk_api.users.get(user_ids= [455547568, 606046649], need_html=1)
-    m = vk_api.get('https://vk.com/id606046649')
-    #s = vk_api.groups.get(cuser_id=9692363, v=5.118)
-    #n = vk_api.groups.getMembers(group_id=name_user, v=5.118)
-    return m #s #, n
-
 
 def token_read():
     with io.open('token.txt') as file:
@@ -123,7 +106,7 @@ async def main():
     print(cpu_num)
     queue = multiprocessing.Queue()
     
-    taskq = CreateTaskQueue(queue, 200,num_ids_chunk=10)
+    taskq = CreateTaskQueue(queue, 2, num_ids_chunk=5)
     taskq.start()
     #async with aiovk.TokenSession(access_token=token_read()) as session: #
     filds ='site,status,type,city,country,contacts,description,wiki_page,start_date,activity,status,age_limits,main_section,members_count,place'
@@ -136,27 +119,12 @@ async def main():
     taskq.join()
     for _ in tasks:
         queue.put(None)
-
+    print('Close')
     for t in tasks:
         t.join()        
+    print('close2')  
         
-        
-        
-    #    api = aiovk.API(session)
-    #    req = ''
-    #    for i in range(45678,45678+100):
-    #        req += str(i)+','
-    #    req += '191527376'
-        #print(req)
-    #    data = await api.groups.getById(group_ids=req, fields=filds)
-        #data2 = await api.database.getCities(country_id=1, offset=100000, count=1000 ,need_all=1)
-        #data1 = await api.groups.getMembers(group_id=191527376)
-        
-    #    for i in data:
-    #        if 'description' in i:
-    #            print(i['name'], i['activity'],'\n', type(i))  
-        #print(data)
-
+  
 if __name__ == "__main__":
     op = OptionParser("Script parsing VK")
     op.add_option("-l", "--log", action="store", default=None, help="logging space")
